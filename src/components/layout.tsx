33fa98bfc7fe559389737323df4b8d9af689cd77
nilsbenz <nils.benz@icloud.com>
2025-06-19 12:27:32 +0200

    chore: redirect all traffic to index on firebase

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,8 @@
 import Footer from "./footer";
-import Header from "./_header";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="flex grow flex-col">
-      <Header />
       {children}
       <Footer />
     </div>
