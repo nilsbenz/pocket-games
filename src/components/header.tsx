cdf6cc2cc29bb55ba57d50be327011127abd807a
nilsbenz <nils.benz@icloud.com>
2025-06-19 12:43:34 +0200

    ui: fix the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,13 @@ import { DicesIcon } from "lucide-react";
 export default function Header() {
   return (
     <>
-      <header className="bg-secondary/70 text-secondary-foreground fixed top-0 z-40 flex h-14 w-full items-center gap-2 border-b-2 px-4 backdrop-blur-md">
-        <DicesIcon strokeWidth={2.5} className="text-primary" />
-        <h1 className="text-lg font-bold">Taschenspiele</h1>
+      <header className="bg-secondary/70 pt-safe-top pl-safe-left pr-safe-right text-secondary-foreground fixed top-0 right-0 left-0 z-40 border-b-2 backdrop-blur-md">
+        <div className="flex h-14 items-center gap-2 px-4">
+          <DicesIcon strokeWidth={2.5} className="text-primary" />
+          <h1 className="text-lg font-bold">Taschenspiele</h1>
+        </div>
       </header>
-      <div className="mb-0.5 h-14" />
+      <div className="mt-safe-top mb-0.5 h-14" />
     </>
   );
 }
